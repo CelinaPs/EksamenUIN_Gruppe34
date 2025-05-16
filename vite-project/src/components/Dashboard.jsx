ac0b55458223422e3b5493db5061e05a2241724d
CelinaPs <143390437+CelinaPs@users.noreply.github.com>
2025-05-16 18:35:29 +0200

    Legger til CategoryPage og Dashboard + litt fler kommentarer

---
+++ vite-project/src/components/Dashboard.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function Dashboard() {
+  const [loggedIn, isLoggedIn] = useState(false);
+
+  const buttonClick = (event) => {
+    //https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event hjuelpet litt med funksjonen.
+    event.preventDefault(); //https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault hjelpet med event.preventDefault()
+    const username = event.target.elements.username.value.trim();
+    if (username !== "") {
+      isLoggedIn(true);
+    }
+  };
+
+  return (
+    <section className="loggin">
+      <h1>{loggedIn ? "Min side" : "Logg inn"}</h1>
+
+      {!loggedIn && (
+        <form onSubmit={buttonClick}>
+          {" "}
+          {/*https://www.w3schools.com/jsref/event_onsubmit.asp hjelpet med onsubmit. */}
+          <label>Brukernavn: </label>
+          <input type="text" name="username"/> {/*Endrer "input" type*/}
+          <button type="submit">Logg inn</button> {/*Knapp til Logg Inn*/}
+        </form>
+      )}
+    </section>
+  );
+}
